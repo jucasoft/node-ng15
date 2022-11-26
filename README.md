489a535b96ec3b088f9d101ed71543b87a3d4a1f
jucasoft <juca@jucasoft.com>
2022-11-27 00:26:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-ng15
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-ng15)
---
+++ index.js
@@ -0,0 +1,3 @@
+cd // run `node index.js` in the terminal
+
+console.log(`Hello Node.js v${process.versions.node}!`);
